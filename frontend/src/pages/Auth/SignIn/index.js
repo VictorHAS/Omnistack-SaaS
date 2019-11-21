c88ed1f60bfb9ca4ac27157c9e9256cd22d8ef21
Victor Hermes <victorhermesalves@gmail.com>
2019-11-20 22:27:30 -0300

    User authentication with redux

--- frontend/src/pages/Auth/SignIn/index.js
+++ frontend/src/pages/Auth/SignIn/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useDispatch } from 'react-redux';
+import AuthActions from '~/store/ducks/auth';
 
 import Button from '~/styles/components/Button';
 import { Container, SignForm } from '../styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(AuthActions.signInRequest(email, password));
+  }
+
   return (
     <Container>
-      <SignForm onSubmit={() => {}}>
+      <SignForm onSubmit={handleSubmit}>
         <h1>Boas vindas</h1>
 
         <span>E-MAIL</span>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
 
         <span>SENHA</span>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
 
         <Button size="big" type="submit">
           Entrar
