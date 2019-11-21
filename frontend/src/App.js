97d85aacd41189590909cb2a49eca6f5fef58c1c
Victor Hermes <victorhermesalves@gmail.com>
2019-11-20 21:34:53 -0300

    Create initial config react-redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import Routes from './routes';
 
 export default function src() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
